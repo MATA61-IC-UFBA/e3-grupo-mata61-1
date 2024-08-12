acd5929b225ccea585d65cda263dba39dc3eccc2
Caio Mendes <36716175+caiosmendes96@users.noreply.github.com>
2024-08-12 14:01:52 -0300

    Add files via upload

--- E3/src/token.h
+++ E3/src/token.h
@@ -3,9 +3,49 @@
 typedef enum {
         ID = 256, 
 	NUM, 
-        KEY,
-        SYM,  
-        SYMC, 
+        ARRAY,
+        //KEY,
+        BOOLEAN, //259
+        CHAR_KEY,  
+        ELSE,
+        FALSE,
+        FOR,
+        FUNCTION,
+        IF,
+        INTEGER,
+        PRINT,
+        RETURN,
+        STRING,
+        TRUE,
+        VOID,
+        WHILE,
+        //SYMC comeca 273
+        LESS_THAN_OR_EQUAL, // <=
+        GREATER_THAN_OR_EQUAL, // >=
+        EQQ, // ==
+        NOT_EQUALS, // !=
+        AND, // &&
+        OR,  // ||
+        //SYM,  
+        ADD, // +
+        MINUS, // -
+        DIV, // /
+        PLUS, // * 
+        PERCENT, // %
+        EXCLAMATION, // !
+        LESS_THAN, //<
+        GREATER_THAN, // >
+        EQUALS, // =
+        COLON, // :
+        SEMICOLON, // ;
+        COMMA, // ,
+        OPEN_PAREN, // (
+        CLOSE_PAREN, // )
+        OPEN_BRACKET,// []
+        CLOSE_BRACKET, // ]
+        OPEN_BRACE, // {
+        CLOSE_BRACE, // }
+        BACKSLASH,  // comentario
         CHAR,
 	STR,
 	ERROR
